5ecac0b8f3fbe4f7a3df6d3d0810e84467456a79
Mehul Arora <mehularora505@gmail.com>
2026-07-18 00:46:29 +0530

    DataBase and Prisma Setup

--- app/page.tsx
+++ app/page.tsx
@@ -9,3 +9,11 @@ export default function Home() {
     </>
   )
 }
+
+/*
+bun add prisma @prisma/client @prisma/adapter-pg
+bunx prisma init => to initialize prizma in our project
+bunx prisma generate => t generate prisma client
+prisma migrate dev
+bunx prisma studio
+*/ 
