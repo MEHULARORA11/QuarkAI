596856a3ecf83db19a461f0159db261ee1e5bd4d
Mehul Arora <mehularora505@gmail.com>
2026-07-25 18:37:38 +0530

    Add suppressHydrationWarning attribute to RootLayout HTML element

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({
     <html
       lang="en"
       className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
+      suppressHydrationWarning
     >
       <body >
         <ClerkProvider>
