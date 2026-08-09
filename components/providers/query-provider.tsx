0c72e60791d77b98a25738ac879643183ad28a74
Mehul Arora <mehularora505@gmail.com>
2026-08-10 03:29:51 +0530

    fix: correct staleTime value in QueryProvider to 2 seconds for improved data fetching

--- components/providers/query-provider.tsx
+++ components/providers/query-provider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"; // hover and see => here QueryClient is a class and QueryClientProvider is a function that accepts client and childeren as a prop
 
 import * as React from "react";
 
@@ -8,18 +8,19 @@ import * as React from "react";
  * Provides a TanStack Query client to the React tree (30s default stale time).
  */
 export function QueryProvider({ children }: { children: React.ReactNode }) {
-    const [queryClient] = React.useState(
+    const [queryClient] = React.useState( // why we are doing this => so that on every re-render , our query cache do not get cleaned up as if use simply query = new QueryClient then on every re render a fresh clientis generated
         () =>
             new QueryClient({
                 defaultOptions: {
                     queries: {
-                        staleTime: 30 * 1000, // 30 seconds // that after how much time ou need to fetch the fresh data 
+                        staleTime: 2 * 1000, // 30 seconds // that after how much time ou need to fetch the fresh data 
                     },
                 },
             })
     );
 
     return (
+
         <QueryClientProvider client={queryClient}>
             {children}
 
